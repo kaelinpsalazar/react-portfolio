bb5db4ee8d33b29ec73105d7e96d3658e2584d0e
Kaelin Salazar <kaelinpsalazar@gmail.com>
2022-10-19 12:08:14 -0600

    set up the routing between pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Contact from "./components/Contact";
+import Biography from "./components/Biography"
+import Projects from "./components/Projects"
+import { useState } from "react";
+
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("HomePage");
+  function render() {
+    if (currentPage === "Biography") {
+      return <Biography />;
+    } else if (currentPage === "Contact") {
+      return <Contact />;
+    } else if (currentPage === "Projects") {
+      return <Projects />;
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar setCurrentPage={setCurrentPage} />
+      <div class="renderedData ">{render()}</div>
+      <Footer />
+    </>
   );
 }
 
